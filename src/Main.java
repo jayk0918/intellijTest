3cd994e6f034b548bc7bac4fbe7f19b600d60e47
Mocking_jayk0918 <jesper2able@gmail.com>
2022-06-19 21:25:54 +0900

    1.0(intellijTest 22.06.19 21:25)

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 0;
+        if (num == 1) {
+            System.out.println("1");
+        } else if (num == 2) {
+            System.out.println("2");
+        } else {
+            System.out.println("0");
+        }
+
+        String test = "Testing console";
+        System.out.println(test);
+
+        System.out.println("Testing Github Commit&Push");
+    }
+}
+
